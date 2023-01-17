58c1c6c2f97e5365fbed64849a7c9694719e21c0
Sakib-Anjum-313 <sakibanjum.313@gmail.com>
2023-01-17 15:04:59 +0600

    Cors promlem

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+
 
 
 const catagories = require('./data/categories.json');
@@ -21,7 +21,14 @@ app.get("/all-courses", (req, res) => {
   res.send(courses);
 });
 
-
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const category_courses = courses.filter(
+    (course) => course.category_id === id
+  );
+  // console.log(category_courses);
+  res.send(category_courses);
+});
 
 
 app.listen(port, () => {
