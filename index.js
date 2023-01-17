7babaf35ef936b4d9879b65643733008c1c05c1f
Sakib-Anjum-313 <sakibanjum.313@gmail.com>
2023-01-17 15:00:49 +0600

    Category done

--- index.js
+++ index.js
@@ -17,18 +17,7 @@ app.get('/course-categories', (req, res) => {
     res.send(catagories);
 });
 
-app.get("/all-courses", (req, res) => {
-    res.send(courses);
-});
 
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    const category_courses = courses.filter((course) => course.category_id === id);
-    
-    console.log(category_courses);
-    res.send(category_courses);
-
-})
 
 
 app.listen(port, () => {
